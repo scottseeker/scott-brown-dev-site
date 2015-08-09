18bd1f736ca772c5f423b2150f9f68ca31ba118c
Scott Brown <scottseeker@gmail.com>
2015-08-09 22:46:27 +0000

    Finished Lesson.

--- client/js/myscript.js
+++ client/js/myscript.js
@@ -1,8 +1,82 @@
-(function() {
+$(function(){
+    
     'use strict';
-
-    $('#featured').carousel({
-        interval: false
+    
+    var topoffset = 50;
+    var slideqty = $('#featured .item').length;
+    var wheight = $(window).height();// get the height of the window
+    var randSlide = Math.floor(Math.random()*slideqty);
+    
+    $('#featured .item').eq(randSlide).addClass('active');
+    
+    $('.fullheight').css('height', wheight); // set window to height
+    
+    // replace IMG inside carousel with bckg image
+    $('#featured .item img').each(function(){
+        var imgSrc = $(this).attr('src');
+        console.log(imgSrc);
+        $(this).parent().css({'background-image': 'url(' + imgSrc +')'});
+        $(this).remove();
     });
-
-}());
+    
+    //adjust height of .fullheight elements
+    $(window).resize(function(){
+       wheight = $(window).height();// get the height of the window
+       $('.fullheight').css('height', wheight); // set window to height
+    });
+    
+    // activate scroll spy
+    $('body').scrollspy({
+        target: 'header .navbar',
+        offset: topoffset
+    });
+    
+    // add inbody class
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+       $('header nav').addClass('inbody'); 
+    } else {
+       $('header nav').removeClass('inbody');
+    }
+    
+    // Add an inbody class to nav 
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
+        var hash = $(this).find('li.active a').attr('href');
+        if(hash !== '#featured') {
+           $('header nav').addClass('inbody'); 
+        } else {
+           $('header nav').removeClass('inbody');
+        }
+    });
+    
+    //Use smooth scrolling when clicking on navigation
+    $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+    }); //smooth scrolling
+    
+    //Automatically generate carousel indicators
+    for (var i = 0; i < slideqty; i++) {
+        var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
+            if(i === randSlide) {
+                insertText += 'class="active"';
+            }
+        insertText += '></li>';
+        $('#featured ol').append(insertText);
+    }
+    
+    $('.carousel').carousel({
+        pause: false
+    });
+    
+});
