a4f96d910f85b05448e0e86342883a7ec24c32fe
Scott Brown <scottseeker@gmail.com>
2015-08-24 00:16:16 +0000

    removed sts-app test

--- client/stsguide/sts-app/shared/services/guide-data.module.js
+++
@@ -1,5 +0,0 @@
-(function(){
-    //sts guide data service (AJAX calls etc.)
-    angular.module('guideDataService', [])
-        .constant('GUIDE_DATA_URL', '/data/channels-data.json');
-})();
--- client/stsguide/sts-app/shared/services/guide.module.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    'use strict';
-
-    //sts guide general service (click handlers, display etc.)
-    angular.module('guideService', ['guideDataService']);
-
-})();
